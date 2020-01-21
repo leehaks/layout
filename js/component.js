a0a93b4f28a05fd32c16bdd518c79899575ba1ae
leejunghak <jhlee@bitnine.net>
2020-01-21 21:07:57 +0900

    화면수정진행

--- js/component.js
+++ js/component.js
@@ -3,7 +3,9 @@ $( document ).ready( function() {
     $("header").load("../html/header.html")
     $("#sidebar").load("../html/sidebar.html")
 
+
     $("#content").load("../html/button.html")
+    $("#anchor").load("../html/anchor.html")
     
 }); 
 
